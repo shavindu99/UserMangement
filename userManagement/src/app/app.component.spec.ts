1389cc86088ee517748bcb92617edd820c69faa8
shavindu99 <shavindu99@gmail.com>
2020-02-13 19:40:39 +0530

    Add

--- userManagement/src/app/app.component.spec.ts
+++ userManagement/src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'tweet'`, () => {
+  it(`should have as title 'userManagement'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('tweet');
+    expect(app.title).toEqual('userManagement');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to tweet!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to userManagement!');
   });
 });
